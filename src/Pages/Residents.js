df3ba7e5ed4c1eadfb3b0d4fe5e65b59bbbbc00e
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-05-15 18:31:21 -0700

    added css files and code

---
+++ src/Pages/Residents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Residents() {
+  return (
+    <div className="Residents">
+      <h1>Residents</h1>
+      <ul>
+        <li>Open Mic @ Club Vibe - Fri 9PM</li>
+        <li>DJ Night @ The Lounge - Sat 10PM</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Residents;
