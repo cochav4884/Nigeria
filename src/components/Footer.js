e11e58ae72b7258455245519dc3bc690de33051c
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-06-19 06:43:51 -0700

    fixed footer!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,28 +13,33 @@ function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} | MFCP & ROC</p>
-        <p>
+        <p className="date">&copy; {new Date().getFullYear()} | MFCP & ROC</p>
+        <p className="date">
           Creator: <NavLink to="/cp">Corinne Padilla</NavLink>
         </p>
-        <p>
+        <p className="date">
           Photographer: <NavLink to="/roc">Richie Osborne</NavLink>
         </p>
-        <p>
+        <p className="date">
           Pastor: <NavLink to="/pastor">Amos Onogwu</NavLink>
         </p>
-
-        <button className="back-to-top" onClick={scrollToTop} aria-label="Back to top">
-          ↑ Back to Top
-        </button>
-
-        <div className="footer-nav">
-          <NavLink to="/privacypolicy">Privacy Policy</NavLink> |{" "}
-          <NavLink to="/termsofuse">Terms of Use</NavLink> |{" "}
-          <NavLink to="/credits">Credits</NavLink> |{" "}
-          <NavLink to="/contact">Contact</NavLink>
-        </div>
+        <p className="date">All rights reserved.</p>
       </div>
+      <button
+        className="back-to-top"
+        onClick={scrollToTop}
+        aria-label="Back to top"
+      >
+        ↑ Back to Top
+      </button>
+
+      <div className="footer-nav">
+        <NavLink to="/privacypolicy">Privacy Policy</NavLink> |{" "}
+        <NavLink to="/termsofuse">Terms of Use</NavLink> |{" "}
+        <NavLink to="/credits">Credits</NavLink> |{" "}
+        <NavLink to="/contact">Contact</NavLink>
+      </div>
+
       <div className="footer-image-container">
         <img src={lookingOut} alt="Looking out from the church building" />
       </div>
